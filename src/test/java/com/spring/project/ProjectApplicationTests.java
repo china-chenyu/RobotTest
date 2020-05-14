7493e586d334baf8ecb7854c8191cd33df0e6ca6
china-chenyu <lyp2217210049@qq.com>
2020-05-14 10:18:31 +0800

    tj

--- src/test/java/com/spring/project/ProjectApplicationTests.java
+++ src/test/java/com/spring/project/ProjectApplicationTests.java
@@ -11,7 +11,6 @@ class ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
-		//21421
 		String[] arr = {"1","2"};
 		Stream<String> arr1 = Stream.of(arr);
 		arr1.forEach(System.out::print);
